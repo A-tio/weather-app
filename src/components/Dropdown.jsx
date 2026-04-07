ce814143b47e67d14abaef85b917035d95396a5e
A-tio <alyssa.frances.08@gmail.com>
2026-04-07 23:45:32 +0800

    basic setup

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,7 +1,47 @@
-import React, { Component } from "react";
+import { useState } from "react";
 
-export default class Dropdown extends Component {
-  render() {
-    return <div> textInComponent </div>;
-  }
+function Dropdown({ title, icon1, icon2, items }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="relative inline-block text-left">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        // I added gap-2 here to put space between the icons and text
+        className="inline-flex justify-center items-center gap-2 w-full rounded-md border p-2"
+      >
+        {/* Changed size to w-5 and added an alt attribute */}
+        <img src={icon1} className="w-5 h-auto" />
+
+        <span>{title}</span>
+
+        <img src={icon2} className="w-5 h-auto" />
+      </button>
+
+      {isOpen && (
+        <div className="absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-white border border-gray-100 z-10">
+          <div className="py-1">
+            {items.map((item, index) => (
+              <a
+                key={index}
+                href="#"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-red-50 hover:text-red-600"
+              >
+                {item}
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Dropdown;
+{
+  /* // <div className="flex items-center bg-red-300 w-fit p-2 justify-between rounded-lg">
+    //   <div>{icon1}</div>
+    //   {name}
+    //   <div>{icon2}</div>
+    // </div> */
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,20 @@
-import React, { Component } from "react";
 import Logo from "../assets/images/logo.svg";
+import Dropdown from "./Dropdown";
+import IconDown from "../assets/images/icon-dropdown.svg";
+import IconUnits from "../assets/images/icon-units.svg";
 
-export default class Navbar extends Component {
-  render() {
-    return (
-      <div className="bg-red-400 flex justify-between py-4 items-center">
-        <img src={Logo} alt="Logo of Weather Now" className="w-64 h-auto " />
-        <div>a </div>
-      </div>
-    );
-  }
+export default function Navbar() {
+  const Unit = ["Celsius", "Fahrenheit", "km/h", "mph"];
+
+  return (
+    <div className="bg-red-400 flex justify-between py-4 items-center">
+      <img src={Logo} alt="Logo of Weather Now" className="w-64 h-auto " />
+      <Dropdown
+        icon1={IconUnits}
+        title="testingww"
+        icon2={IconDown}
+        items={Unit}
+      />
+    </div>
+  );
 }
